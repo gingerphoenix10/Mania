604ff1d4180010d3eb27dae6905dd8772840bb7b
gingerphoenix10 <gingerphoenix10@gmail.com>
2025-09-29 14:34:11 +0100

    Create single levels js for updating levels without updating game

---
+++ levels.js
@@ -0,0 +1,12 @@
+const header = "https://cdn.jsdelivr.net/gh/gingerphoenix10/Mania@main/"
+
+const setlist = [
+  "Supernovae"
+]
+
+for (var song of setlist) {
+  var songUrl = `{header}{song}.js`;
+  var tag = document.createElement("script");
+  tag.src = songUrl;
+  document.body.appendChild(tag);
+}
