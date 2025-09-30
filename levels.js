5982fbac67d20589ebb8c5edaa1e0abecbf017ce
Phoenix Payne-Barnes <gingerphoenix10@gmail.com>
2025-09-30 21:32:04 +0100

    add supernovae solo back

--- levels.js
+++ levels.js
@@ -1,8 +1,9 @@
-const ghProxy = "https://cdn.jsdelivr.net/gh/gingerphoenix10/Mania@main/"
+const ghProxy = "https://cdn.jsdelivr.net/gh/gingerphoenix10/Mania@main/";
 
 const setlist = [
+  "Supernovae",
   "Soulless 5"
-]
+];
 
 for (var song of setlist) {
   var songUrl = `${ghProxy}${song}/chart.js`;
